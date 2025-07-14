d078b3de7ea67fbbecd5e382bcc9dad1b543734f
Sandesh Shinde <sandeshshinde7047@gmail.com>
2025-07-14 18:19:10 +0530

    dotEnv in server

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import routes from "./routes.js";
-
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 
 app.use(cors()); 
