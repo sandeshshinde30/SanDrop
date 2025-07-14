ab519cb0bd038eefc4053f94346b7001f0dc381e
Sandesh Shinde <sandeshshinde7047@gmail.com>
2025-07-14 19:22:38 +0530

    Backend URL

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use("/api", routes);
 
-const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27018/sandropdb";
+const MONGO_URI = "mongodb://localhost:27017/";
 
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log(`Connected to MongoDB at ${MONGO_URI}`))
